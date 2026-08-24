8ec6a2b35c7c75875e25df153d1fd8be273d18fb
braulio rodriguez <br1412922@gmail.com>
2026-08-23 20:54:47 -0600

    Agrega Dockerfile para Render

---
+++ Migrations/20260824024911_MigracionSupabase.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Areneros.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class MigracionSupabase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "LecturasArenero",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Fecha = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Planta = table.Column<string>(type: "text", nullable: false),
+                    Separador = table.Column<string>(type: "text", nullable: false),
+                    Tambor = table.Column<string>(type: "text", nullable: false),
+                    Arenero = table.Column<string>(type: "text", nullable: false),
+                    Estado = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LecturasArenero", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "LecturasHidro",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Fecha = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Planta = table.Column<string>(type: "text", nullable: false),
+                    Bateria = table.Column<string>(type: "text", nullable: false),
+                    Hidrociclon = table.Column<string>(type: "text", nullable: false),
+                    Estado = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LecturasHidro", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "LecturasArenero");
+
+            migrationBuilder.DropTable(
+                name: "LecturasHidro");
+        }
+    }
+}
